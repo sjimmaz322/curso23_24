df60eb7054969678459777f9cea7989c90854de2
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-10-10 10:24:52 +0200

    Seguimos con fechas en php

--- PHP/Octubre/9oct/Fecha1/index.php
+++ PHP/Octubre/9oct/Fecha1/index.php
@@ -1,22 +1,20 @@
 <?php
 
-if(isset($_POST["btnEnviar"])){
-    $error_longi = strlen($_POST["fec1"])!=10||strlen($_POST["fec2"])!=10;
-    $error_formato = substr($_POST["fec1"],2,1)!="/"||substr($_POST["fec2"],2,1)!="/"||substr($_POST["fec1"],5,1)!="/"||substr($_POST["fec2"],5,1)!="/";
-    $error_num1 = substr($_POST["fec1"],0,2)<00||substr($_POST["fec1"],0,2)>31||substr($_POST["fec2"],0,2)<00||substr($_POST["fec2"],0,2)>31;
-    $error_num2 = substr($_POST["fec1"],3,2)<00||substr($_POST["fec1"],3,2)>12||substr($_POST["fec2"],3,2)<00||substr($_POST["fec2"],3,2)>12;
-    $error_num3 = substr($_POST["fec1"],5,4)<1970||substr($_POST["fec1"],5,4)>9999||substr($_POST["fec2"],5,4)<1970||substr($_POST["fec2"],5,4)>9999;
-    $error_num= $error_num1 ||$error_num2 ||$error_num3;
-    $error_fecha = !strtotime($_POST["fec1"]) || !strtotime($_POST["fec2"]);
+if (isset($_POST["btnEnviar"])) {
+    $error_vacio = $_POST["fec1"] = "" || $_POST["fec2"] = "";
+    $error_longi = strlen($_POST["fec1"]) != 10 || strlen($_POST["fec2"]) != 10;
+    $error_formato = substr($_POST["fec1"], 2, 1) != "/" || substr($_POST["fec2"], 2, 1) != "/" || substr($_POST["fec1"], 5, 1) != "/" || substr($_POST["fec2"], 5, 1) != "/";
+    $error_num1 = substr($_POST["fec1"], 0, 2) < 00 || substr($_POST["fec1"], 0, 2) > 31 || substr($_POST["fec2"], 0, 2) < 00 || substr($_POST["fec2"], 0, 2) > 31;
+    $error_num2 = substr($_POST["fec1"], 3, 2) < 00 || substr($_POST["fec1"], 3, 2) > 12 || substr($_POST["fec2"], 3, 2) < 00 || substr($_POST["fec2"], 3, 2) > 12;
+    $error_num3 = substr($_POST["fec1"], 5, 4) < 1970 || substr($_POST["fec1"], 5, 4) > 9999 || substr($_POST["fec2"], 5, 4) < 1970 || substr($_POST["fec2"], 5, 4) > 9999;
+    $error_num = $error_num1 || $error_num2 || $error_num3;
+    $error_fecha = checkdate((substr($_POST["fec1"], 0, 2)), (substr($_POST["fec1"], 3, 2)), (substr($_POST["fec1"], 5, 4))) || checkdate((substr($_POST["fec2"], 0, 2)), (substr($_POST["fec2"], 3, 2)), (substr($_POST["fec2"], 5, 4)));
 
-    $error_form = $error_longi || $error_formato || $error_num || $error_fecha;
-
+    $error_form = $error_vacio || $error_longi || $error_formato || $error_num || $error_fecha;
 }
 
-if(isset($_POST["btnEnviar"])&&!$error_form){
+if (isset($_POST["btnEnviar"]) && !$error_form) {
     require "vistas/vistaRespuesta.php";
-
-}else{
+} else {
     require "vistas/vistaFormulario.php";
 }
-?>
