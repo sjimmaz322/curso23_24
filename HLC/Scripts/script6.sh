88d4182ebf224e191d3cd91228f3cb1f916e4a06
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-11-06 11:11:55 +0100

    Miau

--- HLC/Scripts/script6.sh
+++ HLC/Scripts/script6.sh
@@ -2,7 +2,7 @@
 
 filename='archivo5.txt.gz'
 
-gunzip -c $filename | cut -d " " -f 1-3 $filename | while read -r col1 col2 col3; do
+gunzip -c $filename | cut -d " " -f 1-3 | while read -r col1 col2 col3; do
     echo "Columna 1: $col1"
     echo "Columna 2: $col2"
     echo "Columna 3: $col3"
