807e16b643b5c774e8e9693896447632bb49d065
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-11-23 18:57:14 +0100

    Añadido reactstrap y plantilla

--- JS/React/plantilla/src/App.js
+++ JS/React/plantilla/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Button } from 'reactstrap';
+/*
+function Boton(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Button onClick={() => props.cambia()}>
+      ingles.
+    </Button>
+  )
+}
+*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      /* Aquí van los estados */
+    }
+  }
+ 
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
