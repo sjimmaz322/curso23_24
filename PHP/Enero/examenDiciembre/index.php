c4da10fb6b46a33c64d8d727755ec7a94544f3dc
sjimmaz322 <sjimmaz322@g.educaand.es>
2024-01-08 10:01:57 +0100

    Miau

---
+++ PHP/Enero/examenDiciembre/index.php
@@ -0,0 +1,24 @@
+<?php
+session_name("examen3_23_24");
+session_start();
+
+if (isset($_SESSION["usuario"])) {
+    //Estoy logeado
+} else {
+    //No estoy logeado
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen Diciembre</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
